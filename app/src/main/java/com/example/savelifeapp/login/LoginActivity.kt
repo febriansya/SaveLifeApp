82d6ae750fdc83afca3fef2dd78b38a767f5aa7e
yan febriansyah <44943550+febriansya@users.noreply.github.com>
2022-04-15 14:12:44 +0800

    spashscreen

--- app/src/main/java/com/example/savelifeapp/login/LoginActivity.kt
+++ app/src/main/java/com/example/savelifeapp/login/LoginActivity.kt
@@ -2,13 +2,17 @@ package com.example.savelifeapp.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.findNavController
 import com.example.savelifeapp.R
+import com.example.savelifeapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
 
     }
---
+++ app/src/main/java/com/example/savelifeapp/splashScreen/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.example.savelifeapp.splashScreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.savelifeapp.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
