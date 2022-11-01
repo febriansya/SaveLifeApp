61d9fe41ceaf77fdee855bc677c3b6b62374f31d
yan febriansyah <44943550+febriansya@users.noreply.github.com>
2022-11-01 10:50:41 +0800

    Cicilan Mengerjakan Fiture DetailMyRequestAdapter.kt

---
+++ app/src/main/java/com/example/savelifeapp/ui/request/viewpager/mrequest/DetailMyRequestActivity.kt
@@ -0,0 +1,11 @@
+package com.example.savelifeapp.ui.request.viewpager.mrequest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailMyRequestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_my_request)
+    }
+}
